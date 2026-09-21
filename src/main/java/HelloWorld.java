99a84a0ad902e05167a0748a67a182dd8778389e
naveenm145 <2022wb86145@wilp.bits-pilani.ac.in>
2026-09-22 02:57:48 +0530

    Update output message in HelloWorld class

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Jenkins Continuous Integration");
-
+        System.out.println("Hello Jenkins CI/CD");
+    }
+}
     
